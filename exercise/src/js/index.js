972350912254305686fad86e89597944dc267ec8
Miguel Molina <miguelmolina.1990@gmail.com>
2022-06-13 20:05:25 -0500

    pokemon excersice

---
+++ exercise/src/js/index.js
@@ -0,0 +1,13 @@
+const data = require('../data/data.json');
+
+const searchPokemon = (name) => {
+    const pokemon = data.results.find(pokemon => pokemon.name === name);
+    if (pokemon) {
+        console.log('Pokemon encontrado');
+        console.log(`URL: ${pokemon.url}`);     
+    } else {
+        console.log(`No se encontró un pokemon con el nombre ${name}`);
+    }
+};
+
+searchPokemon('limber');
